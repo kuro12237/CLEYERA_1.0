6270f0912b893ef8cd0049068af007b4b12c7a87
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-12-10 02:11:26 +0900

    a

--- CLEYERA/Utility/CollisionManager/CollisionManager.cpp
+++ CLEYERA/Utility/CollisionManager/CollisionManager.cpp
@@ -11,6 +11,7 @@ void CollisionManager::CheckAllCollision()
 	std::list<AABBCollider*>::iterator itrAABB = aabbColliders_.begin();
 	std::list<OBBCollider*>::iterator itrOBB = obbColliders_.begin();
 
+	// list
 	// push_backしてないので、情報が入ってない
 	// この状態で衝突判定するとエラーはいる
 	// 後で処理の中にif文作って、情報は言ってなかったら抜けるようにする
